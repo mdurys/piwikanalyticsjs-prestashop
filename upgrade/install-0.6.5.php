d1a9ca16a75515724aca5e9c46aa5f07f684d06c
Christian M. Jensen <christian@cmjscripter.net>
2014-10-07 12:22:25 +0200

    wrong wring wrong ....

--- upgrade/install-0.6.5.php
+++ upgrade/install-0.6.5.php
@@ -9,7 +9,7 @@ function upgrade_module_0_6_5($module) {
     Configuration::updateValue('PIWIK_COOKIE_DOMAIN', '*.' . str_replace('www.', '', Tools::getShopDomain()));
     Configuration::updateValue('PIWIK_SET_DOMAINS', Tools::getShopDomain());
     Configuration::updateValue('PIWIK_DNT', 1);
-    Configuration::updateValue('PIWIK_PROXY_SCRIPT', str_replace("http://", '', piwikanalyticsjs::getModuleLink($this->name, 'piwik')));
+    Configuration::updateValue('PIWIK_PROXY_SCRIPT', str_replace("http://", '', piwikanalyticsjs::getModuleLink($module->name, 'piwik')));
 
     return true;
 }
