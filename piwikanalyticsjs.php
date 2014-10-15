66cb4ec04a3abb72d9b6f6689590348a3855c140
Christian M. Jensen <christian@cmjdesign.dk>
2014-10-15 09:02:32 +0200

    Update licenses to GPL v3 +gpl.txt
    Update licenses to GPL v3
    Fix incompetent Windows line endings

--- piwikanalyticsjs.php
+++ piwikanalyticsjs.php
@@ -6,19 +6,24 @@ if (!defined('_PS_VERSION_'))
 /*
  * Copyright (C) 2014 Christian Jensen
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * This file is part of PiwikAnalyticsJS for prestashop.
+ * 
+ * PiwikAnalyticsJS for prestashop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PiwikAnalyticsJS for prestashop is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with PiwikAnalyticsJS for prestashop.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @link http://cmjnisse.github.io/piwikanalyticsjs-prestashop
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
 /* Backward compatibility */
--- views/templates/hook/jstracking.tpl
+++ views/templates/hook/jstracking.tpl
@@ -1,3 +1,26 @@
+{*
+ * Copyright (C) 2014 Christian Jensen
+ *
+ * This file is part of PiwikAnalyticsJS for prestashop.
+ * 
+ * PiwikAnalyticsJS for prestashop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * PiwikAnalyticsJS for prestashop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with PiwikAnalyticsJS for prestashop.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @link http://cmjnisse.github.io/piwikanalyticsjs-prestashop
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+*}
+
 <script type="text/javascript">
         {literal}var u=(("https:" == document.location.protocol) ? "https://{/literal}{$PIWIK_HOST}{literal}" : "http://{/literal}{$PIWIK_HOST}{literal}");
         var _paq = _paq || [];
